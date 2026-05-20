8bebef4b7ab9068dc039f0c0db6e996ae399b057
SimonSCWong <132118540+SimonSCWong@users.noreply.github.com>
2026-05-20 13:07:55 +0800

    Update views.py

--- views.py
+++ views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import CustomUser
 from django.contrib.auth import logout
-from django.contrib.auth import index
 
 # from .models import Booking  # Assuming you have a Booking model defined
 # hotels/views.py
